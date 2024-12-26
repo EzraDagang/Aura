537d1a32ddc47cac1da3a8f1e49418b9dd2599e5
EzraDagang <ezradagang@gmail.com>
2024-12-27 00:40:36 +0800

    Changed "onBoarding" to "onBoardingActivity"

--- app/src/main/java/com/example/aura/onboardingActivity.java
+++ app/src/main/java/com/example/aura/onboardingActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class onboarding extends AppCompatActivity {
+public class onboardingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
