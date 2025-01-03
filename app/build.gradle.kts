a29e7155b7aaabcf561b385c191e5760a37f4e69
yitong0510 <yitongtan0510@gmail.com>
2025-01-03 15:43:45 +0800

    added firebase bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,7 +40,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.glide)  // For Glide
     annotationProcessor(libs.glide.compiler) // For Glide Compiler
-
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
