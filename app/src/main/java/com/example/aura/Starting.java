d1d867dbc734a806d105dae2708896a5f945ae7f
yanjie <yanjie0910@gmail.com>
2025-01-04 15:42:36 +0800

    start button to discover page--yanjie

--- app/src/main/java/com/example/aura/Starting.java
+++ app/src/main/java/com/example/aura/Starting.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.aura.Courses.DiscoverScreen;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,19 +20,35 @@ public class Starting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Yi Tong commented on Starting.java
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
 
+        // Find the Danger button by its ID
         final Button btnDanger = findViewById(R.id.BtnDanger);
 
-        btnDanger.setOnClickListener((new View.OnClickListener() {
+        // Set an OnClickListener for the Danger button
+        btnDanger.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                // Create an Intent to navigate to the Emergency activity
                 Intent i = new Intent(getApplicationContext(), Emergency.class);
                 startActivity(i);
             }
-        }));
-    }
+        });
 
+        // Find the Start button by its ID
+        final Button btnStart = findViewById(R.id.BtnStart);
+
+        // Set an OnClickListener for the Start button
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to navigate to the NextPageActivity
+                Intent i = new Intent(getApplicationContext(), DiscoverScreen.class);
+                startActivity(i);
+            }
+        });
+    }
 }
+
+
